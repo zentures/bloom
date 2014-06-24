37d87b33c55cd0a53b65d00280b800124c056754
Jian Zhen <zhenjl@gmail.com>
2014-06-24 14:50:06 -0700

    updated copyright and go fmt

--- standard/standard_test.go
+++ standard/standard_test.go
@@ -1,8 +1,16 @@
-/*
- * Copyright (c) 2013 Zhen, LLC. http://zhen.io. All rights reserved.
- * Use of this source code is governed by the MIT license.
- *
- */
+// Copyright (c) 2014 Dataence, LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package standard
 
@@ -11,14 +19,15 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"fmt"
-	"github.com/reducedb/bloom"
-	"github.com/reducedb/cityhash"
-	"github.com/spaolacci/murmur3"
 	"hash"
 	"hash/crc64"
 	"hash/fnv"
 	"os"
 	"testing"
+
+	"github.com/dataence/bloom"
+	"github.com/dataence/cityhash"
+	"github.com/spaolacci/murmur3"
 )
 
 var (
